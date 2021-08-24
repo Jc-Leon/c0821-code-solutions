cc654a35a315e5ee2128546303924e9773626dfb
Jc Leon <leonjuan123@yahoo.com>
2021-08-24 11:07:32 -0700

    completed javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,7 +1,7 @@
 var orderHistory = [
   {
     orderPlaced: 'August 4,2020',
-    total: '$34',
+    total: 34,
     shipTo: 'JS Masher',
     orderNum: '19837409184738',
     delivered: 'August 8, 2020',
@@ -9,16 +9,16 @@ var orderHistory = [
       {
         productType: 'book',
         img: 'src',
-        title: 'random title',
-        author: 'random auth',
+        title: 'Javascript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
         return: 'Sept 7, 2020',
-        msrp: '31.55'
+        msrp: 31.55
       }
     ]
   },
   {
     orderPlaced: 'July 19,2020',
-    total: '$44.53',
+    total: 44.53,
     shipTo: 'JS Masher',
     orderNum: '10928319028309',
     delivered: 'July 20, 2020',
@@ -27,16 +27,16 @@ var orderHistory = [
       {
         productType: 'book',
         img: 'src',
-        title: 'random title',
-        author: 'random auth',
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
         return: 'Aug 19, 2020',
-        msrp: '41.33'
+        msrp: 41.33
       }
     ]
   },
   {
     orderPlaced: 'July 4,2020',
-    total: '$17.22',
+    total: 17.22,
     shipTo: 'JS Masher',
     orderNum: '798237452049835',
     delivered: 'July 7, 2020',
@@ -45,15 +45,15 @@ var orderHistory = [
       {
         productType: 'electronic',
         img: 'src',
-        title: 'random title',
+        title: 'Gamecube Adapter',
         return: 'Aug 5, 2020',
-        msrp: '15.98'
+        msrp: 15.98
       }
     ]
   },
   {
     orderPlaced: 'July 3,2020',
-    total: '$138.93',
+    total: 138.93,
     shipTo: 'JS Masher',
     orderNum: '2809409283',
     delivered: 'July 5, 2020',
@@ -61,17 +61,17 @@ var orderHistory = [
       {
         productType: 'electronic',
         img: 'src',
-        title: 'random title',
+        title: 'Gamecube controller',
         return: 'Aug 4, 2020',
-        msrp: '94.95'
+        msrp: 94.95
       },
       {
         productType: 'book',
         img: 'src',
-        title: 'random title',
-        author: 'random author',
+        title: 'The Art of SQL',
+        author: 'Faroult, Stephanie',
         return: 'Aug 4, 2020',
-        msrp: '33.99'
+        msrp: 33.99
       }
     ]
   }
