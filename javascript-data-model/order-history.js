06cec3d871b181376da384255dec5b0d5e4219a3
Jc Leon <leonjuan123@yahoo.com>
2021-08-24 10:46:09 -0700

    completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,78 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4,2020',
+    total: '$34',
+    shipTo: 'JS Masher',
+    orderNum: '19837409184738',
+    delivered: 'August 8, 2020',
+    items: [
+      {
+        productType: 'book',
+        img: 'src',
+        title: 'random title',
+        author: 'random auth',
+        return: 'Sept 7, 2020',
+        msrp: '31.55'
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 19,2020',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    orderNum: '10928319028309',
+    delivered: 'July 20, 2020',
+    deliverMsg: 'random text',
+    items: [
+      {
+        productType: 'book',
+        img: 'src',
+        title: 'random title',
+        author: 'random auth',
+        return: 'Aug 19, 2020',
+        msrp: '41.33'
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 4,2020',
+    total: '$17.22',
+    shipTo: 'JS Masher',
+    orderNum: '798237452049835',
+    delivered: 'July 7, 2020',
+    deliverMsg: 'random text',
+    items: [
+      {
+        productType: 'electronic',
+        img: 'src',
+        title: 'random title',
+        return: 'Aug 5, 2020',
+        msrp: '15.98'
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 3,2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    orderNum: '2809409283',
+    delivered: 'July 5, 2020',
+    items: [
+      {
+        productType: 'electronic',
+        img: 'src',
+        title: 'random title',
+        return: 'Aug 4, 2020',
+        msrp: '94.95'
+      },
+      {
+        productType: 'book',
+        img: 'src',
+        title: 'random title',
+        author: 'random author',
+        return: 'Aug 4, 2020',
+        msrp: '33.99'
+      }
+    ]
+  }
+];
