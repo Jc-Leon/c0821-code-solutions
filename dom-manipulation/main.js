a3199e31f67ce87863638c0fa111edeba296fee3
Jc Leon <leonjuan123@yahoo.com>
2021-08-30 16:32:33 -0700

    completed dom-manipulation

---
+++ dom-manipulation/main.js
@@ -0,0 +1,44 @@
+var $number = 0;
+var $button = document.querySelector('.hot-button');
+var $click = document.querySelector('.click-count');
+
+$button.addEventListener('click', function (event) {
+  $number++;
+
+  $click.textContent = 'Clicks: ' + $number;
+
+  if ($number < 4) {
+    $button.className = 'hot-button ' + 'cold';
+  } else if ($number < 7) {
+    $button.className = 'hot-button ' + 'cool';
+  } else if ($number < 10) {
+    $button.className = 'hot-button ' + 'tepid';
+  } else if ($number < 13) {
+    $button.className = 'hot-button ' + 'warm';
+  } else if ($number < 16) {
+    $button.className = 'hot-button ' + 'hot';
+  } else {
+    $button.className = 'hot-button ' + 'nuclear';
+  }
+});
+
+// function handleClick(event) {
+//   $number++;
+
+//   $click.textContent = 'Clicks: ' + $number;
+
+//   if ($number < 4) {
+//     $button.className = 'hot-button ' + 'cold';
+//   } else if ($number < 7) {
+//     $button.className = 'hot-button ' + 'cool';
+//   } else if ($number < 10) {
+//     $button.className = 'hot-button ' + 'tepid';
+//   } else if ($number < 13) {
+//     $button.className = 'hot-button ' + 'warm';
+//   } else if ($number < 16) {
+//     $button.className = 'hot-button ' + 'hot';
+//   } else {
+//     $button.className = 'hot-button ' + 'nuclear';
+//   }
+// }
+// $button.addEventListener('click', handleClick);
