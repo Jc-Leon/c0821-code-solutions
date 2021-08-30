bdeabbb674261f33e849f1184ccbe6cf7b932e5e
Jc Leon <leonjuan123@yahoo.com>
2021-08-30 13:25:44 -0700

    completed array-manipulation-1

--- array-manipulation-1/src/initial.js
+++ array-manipulation-1/src/initial.js
@@ -1,7 +1,14 @@
 /* exported initial */
 function initial(array) {
-  array.pop();
-  return array;
+  var arr = [];
+  for (var i = 0; i < array.length - 1; i++) {
+    arr.push(array[i]);
+  }
+  return arr;
 }
-// pop method to return the last element of the array
-// return array from the function
+// create a new arr as an empty array
+// create for loop with initialization var i is = 0
+// with the condition if i is less than the array at the last indexed
+// with final expression i being incremented by 1
+// then push the array at its index to the new arr
+// then return the new arr from the function
