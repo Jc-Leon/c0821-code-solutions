9674272d437998555586b25241c3f4bfe25d69b8
Jc Leon <leonjuan123@yahoo.com>
2021-09-01 11:12:10 -0700

    completed javascript-forms

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -3,14 +3,14 @@ var $form = document.querySelector('#contact-form');
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  var $name = $form.name.value;
-  var $email = $form.email.value;
-  var $message = $form.message.value;
+  var name = $form.name.value;
+  var email = $form.email.value;
+  var message = $form.message.value;
 
   var obj = {
-    name: $name,
-    email: $email,
-    message: $message
+    name: name,
+    email: email,
+    message: message
   };
 
   console.log(obj);
