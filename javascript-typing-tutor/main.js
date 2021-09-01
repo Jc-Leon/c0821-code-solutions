d95b95a5ab7706c3e97300b40a07af40d100024a
Jc Leon <leonjuan123@yahoo.com>
2021-08-31 23:15:27 -0700

    progress

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,16 @@
+var $span = document.querySelectorAll('span');
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+  for (var i = 0; i < $span.length; i++) {
+
+    if ($span[0] === event.code[i]) {
+      $span.className = 'correct';
+
+    }
+    console.log('hello', event.code);
+
+  }
+}
+console.log('nodelist', $span[0]);
