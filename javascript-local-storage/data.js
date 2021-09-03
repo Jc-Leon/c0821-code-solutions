296d402397f165e3b89ae8c183bc41c51a36797d
Jc Leon <leonjuan123@yahoo.com>
2021-09-03 13:22:30 -0700

    completed javascript-local-storage

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -5,11 +5,10 @@ var todos = [];
 function handleStorage(event) {
   var todosJSON = JSON.stringify(todos);
   localStorage.setItem('javascript-local-storage', todosJSON);
-  var previousTodosJSON = localStorage.getItem('javascript-local-storage');
-
-  if (previousTodosJSON !== null) {
-    todos = JSON.parse(previousTodosJSON);
-  }
 }
+var previousTodosJSON = localStorage.getItem('javascript-local-storage');
 
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
 window.addEventListener('beforeunload', handleStorage);
