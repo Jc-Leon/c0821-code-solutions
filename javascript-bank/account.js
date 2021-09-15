cdd2057bc54d10edf0df3499e220cd4c53b3a69c
Jc Leon <leonjuan123@yahoo.com>
2021-09-15 10:02:21 -0700

    progress

---
+++ javascript-bank/account.js
@@ -0,0 +1,38 @@
+/* exported Account */
+function Account(number, holder) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
+}
+
+Account.prototype.deposit = function (amount) {
+  if (amount <= 0 || !Number.isInteger(amount)) {
+    return false;
+  } else {
+    var newTransaction = new Transaction('deposit', amount);
+    this.transactions.push(newTransaction);
+    return true;
+  }
+};
+
+Account.prototype.withdraw = function (amount) {
+  if (amount <= 0 || !Number.isInteger(amount)) {
+    return false;
+  } else {
+    var newTransaction = new Transaction('withdrawal', amount);
+    this.transactions.push(newTransaction);
+    return true;
+  }
+};
+
+Account.prototype.getBalance = function () {
+  var balance = 0;
+  for (var i = 0; i < this.transactions.length; i++) {
+    var transaction = this.transactions[i];
+
+    if (transaction.type === 'deposit') {
+
+    }
+
+  }
+};
