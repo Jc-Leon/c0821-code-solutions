31fb6300ed2cdc10317e05bbf370365659d8cd60
Jc Leon <leonjuan123@yahoo.com>
2021-09-14 17:41:05 -0700

    completed string-manipulation-2

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,4 +1,7 @@
 /* exported capitalizeWords */
 function capitalizeWords(string) {
+  var str = string.toLowerCase().split(' ');
+  for (var i = 0; i < string.length; i++) {
 
+  }
 }
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,11 @@
 /* exported numVowels */
+function numVowels(string) {
+  var totalVowels = 0;
+  var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  for (var char of string) {
+    if (vowels.includes(char)) {
+      totalVowels++;
+    }
+  }
+  return totalVowels;
+}
