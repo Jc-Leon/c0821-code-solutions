cf69708f9fa30a6510d5c2736944a1f4c80e592e
Jc Leon <leonjuan123@yahoo.com>
2021-09-27 23:40:55 -0700

    completed object-manipulation-2

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,4 +1,14 @@
 /* exported invert */
 function invert(source) {
-
+  var obj = {};
+  for (var keys in source) {
+    obj[source[keys]] = keys;
+  }
+  return obj;
 }
+/*
+create new object literal
+loop the keys in the source object
+then assign properties of value of the source at keys to the new object literal
+then assign the key to the value of the new object key
+*/
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,8 +1,19 @@
 /* exported pick */
 function pick(source, keys) {
   var obj = {};
-  console.log(source);
-  console.log(keys);
+  for (var i = 0; i < keys.length; i++) {
+    var keyValue = keys[i];
+    if (source[keyValue] !== undefined) {
+      obj[keyValue] = source[keyValue];
+    }
+  }
 
   return obj;
 }
+/* create a new object literal
+checked every key at its index and grabbed the value
+and assign the value to new var named keyValue
+check to see if the source object at the keyValue is not equal to undefined
+if the condition is true then assign the source object at its keyvalue to the
+new obj at its keyvalue
+*/
