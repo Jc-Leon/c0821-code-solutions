510d7dddbe7e2857a497d524566d5f702eec6cb4
Jc Leon <leonjuan123@yahoo.com>
2021-09-28 11:47:05 -0700

    progress

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,12 +1,19 @@
 /* exported lastChars */
-// function lastChars(length, string) {
-//   var newStr = '';
-//   if (length > string.length - 1) {
-//     return string;
-//   }
-//   for (var i = 0; i < length; i++) {
-//     newStr += string[i];
-//   }
+function lastChars(length, string) {
+  var newStr = string;
+  var last = newStr.substr(newStr.length - length);
+  console.log(newStr);
+  console.log(length);
+  console.log(newStr.length);
+  // console.log(length);
+  // console.log(string);
+  // if (length > string.length) {
+  //   return string;
+  // }
+  // for (var i = string.length - 1; i >= length; i--) {
+  //   newStr += string[i];
+  //   console.log
+  // }
 
-//   return newStr;
-// }
+  return last;
+}
