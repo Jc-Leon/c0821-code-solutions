730fe4e0afe62472edcb6464a35476760741fa60
Jc Leon <leonjuan123@yahoo.com>
2021-09-29 10:50:40 -0700

    completed object-manipulation-2

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -11,7 +11,7 @@ function pick(source, keys) {
   return obj;
 }
 /* create a new object literal
-checked every key at its index and grabbed the value
+checked every key at its index and grab the value
 and assign the value to new var named keyValue
 check to see if the source object at the keyValue is not equal to undefined
 if the condition is true then assign the source object at its keyvalue to the
