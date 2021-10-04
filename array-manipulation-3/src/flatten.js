be6b16a42025800a26b445e0f6f3359e96b7b715
Jc Leon <leonjuan123@yahoo.com>
2021-10-04 14:35:26 -0700

    progress

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -2,7 +2,23 @@
 function flatten(array) {
   var newarr = [];
   for (var i = 0; i < array.length; i++) {
-    console.log(array[i]);
+    var value = array[i];
+    if (Array.isArray(value)) {
+      for (var j = 0; j < value.length; j++) {
+        newarr.push(value[j]);
+      }
+    } else {
+      newarr.push(value);
+    }
   }
   return newarr;
 }
+
+/*
+create an array literal
+loop through the array to get the indexes
+create a condition that if the index is array within the original array then
+loop through the index array to get the values of the indexed array
+and push the values into the new array literal
+else if the array is not in an array then push those values to the new array
+*/
--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,10 @@
 /* exported union */
+function union(first, second) {
+  var newArr = [];
+  for (var i = 0; i < first.length; i++) {
+    console.log(first[i]);
+    for (var j = 0; j < second.length; j++) {
+      console.log(second[j]);
+    }
+  }
+}
