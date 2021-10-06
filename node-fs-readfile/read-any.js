a0acaf63989b48a1b875f7eea650eb0ada469362
Jc Leon <leonjuan123@yahoo.com>
2021-10-06 15:44:00 -0700

    completed node-fs-readfile

---
+++ node-fs-readfile/read-any.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const file = process.argv[2];
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
