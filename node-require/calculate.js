375012694647aa06d5c16f6c4691cf7a54b70128
Jc Leon <leonjuan123@yahoo.com>
2021-10-07 00:12:36 -0700

    completed node-require

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -3,18 +3,16 @@ const subtract = require('./subtract');
 const divide = require('./divide');
 const multiply = require('./multiply');
 
-const x = parseFloat(process.argv0[2]);
-const operation = parseFloat(process.argv0[3]);
-const y = parseFloat(process.argv0[4]);
+const x = parseInt(process.argv[2]);
+const operator = process.argv[3];
+const y = parseInt(process.argv[4]);
 
-if (operation === 'plus') {
+if (operator === 'plus') {
   console.log('result: ', add(x, y));
-} else if (operation === 'subtract') {
+} else if (operator === 'subtract') {
   console.log('result: ', subtract(x, y));
-} else if (operation === ' divide') {
-  console.log('result: ', divide(x, y));
-} else if (operation === 'times') {
+} else if (operator === 'times') {
   console.log('result: ', multiply(x, y));
-} else {
-  console.log('Not like this ');
+} else if (operator === 'divide') {
+  console.log('result: ', divide(x, y));
 }
