3506ab56bfaf5195bf0b43d0eea07b4e93e9932a
Jc Leon <leonjuan123@yahoo.com>
2021-10-08 02:27:45 -0700

    completed node-notes-cli

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,4 +1,57 @@
-const read = require('./read');
+const fs = require('fs');
+const command = process.argv.splice(2);
+const file = require('./data.json');
 
-// console.log(process.argv);
-console.log(read);
+function read() {
+  for (const key in file.notes) {
+    console.log(`${key}: ${file.notes[key]}`);
+  }
+}
+
+function add() {
+  file.notes[file.nextId] = command[1];
+  file.nextId++;
+  fs.writeFile('data.json', JSON.stringify(file, null, 2), 'utf8', err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+function remove() {
+  if (!file.notes[command[1]]) {
+    console.log(`You really think there is a ${command[1]}?`);
+    return;
+  }
+  delete file.notes[command[1]];
+  fs.writeFile('data.json', JSON.stringify(file, null, 2), 'utf8', err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+function update() {
+  if (!file.notes[command[1]]) {
+    console.log(`You really think there is a ${command[1]}?`);
+    return;
+  }
+  file.notes[command[1]] = command[2];
+  fs.writeFile('data.json', JSON.stringify(file, null, 2), 'utf8', err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+if (command[0] === 'read') {
+  read();
+} else if (command[0] === 'add') {
+  add();
+} else if (command[0] === 'delete') {
+  remove();
+} else if (command[0] === 'update') {
+  update();
+} else {
+  console.log('Enter a valid command');
+}
--- node-notes-cli/update.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const fileName = './file.json';
-const file = require(fileName);
-
-fs.writeFile(fileName, JSON.stringify(file), err => {
-  file.key++;
-  if (err) return console.log(err);
-  console.log(JSON.stringify(file));
-});
