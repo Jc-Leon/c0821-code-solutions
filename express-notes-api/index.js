2626168a7499a837b22bd1b13d409847a217f7e2
Jc Leon <leonjuan123@yahoo.com>
2021-10-12 01:14:29 -0700

    progress

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const file = require('./data.json');
+const fs = require('fs');
+
+app.listen(3000, () => {
+  console.log('Express server listening on port 3000');// eslint-disable-line
+});
 
 app.get('/api/notes', function (req, res) {
   const newarr = [];
   for (const property in file.notes) {
     newarr.push(file.notes[property]);
   }
-  console.log(newarr);
   res.json(newarr);
 
 });
@@ -20,21 +24,31 @@ app.get('/api/notes/:id', function (req, res) {
     res.status(404).json({ error: `cannot find note with id ${id}` });
   } else {
     res.json(file.notes[id]);
-
   }
 });
 app.use(express.json());
 
 app.post('/api/notes', (req, res) => {
+  const empty = {};
   const obj = req.body;
-  console.log(obj);
-  console.log(file);
-  console.log(file.notes);
-  file.notes = obj;
-  console.log(obj);
-  res.status(201).json(obj);
-});
+  console.log('obj', obj);
+  obj.id = file.nextId;
+  console.log('file.nextId', file.nextId);
+  console.log(obj.id);
+  file.notes[file.nextId] = obj;
+  console.log(file.notes[file.nextId]);
+  file.nextId++;
+  if (obj) {
+    console.log(!obj);
+    res.status(400).json({ error: 'content is a required field' });
+  } else {
 
-app.listen(3000, () => {
-  console.log('Express server listening on port 3000');
+    res.status(201).json(obj);
+  }
+
+  fs.writeFile('data.json', JSON.stringify(file, null, 2), 'utf8', err => {
+    if (err) {
+      console.error(err);
+    }
+  });
 });
