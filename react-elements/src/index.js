3e96f1b6df7995a1000b63cf323f4ce2945d06e9
Jc Leon <leonjuan123@yahoo.com>
2021-10-18 16:35:57 -0700

    completed react-elements

---
+++ react-elements/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const word = React.createElement(
+  'h1',
+  [null],
+  ['Hello, React!']
+);
+ReactDOM.render(word, document.getElementById('root'));
